dd4ab19997f10f2e7f676b15463df46cff390b1f
sshariff01 <shoabe.shariff@gmail.com>
2015-02-18 20:39:17 -0500

    Add bare device discovery. Output discovered devices in LogCat.

--- app/src/main/java/com/bluetooth/test/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/bluetooth/test/bluetoothtest/MainActivity.java
@@ -2,32 +2,93 @@ package com.bluetooth.test.bluetoothtest;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends Activity {
     private final static int REQUEST_ENABLE_BT = 1;
+    private List<String> discoveredDevices = new ArrayList<String>();
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                Log.i ("BT_TEST: DEVICE FOUND", "DEVICE NAME: " + device.getName());
+                Log.i ("BT_TEST: DEVICE FOUND", "DEVICE ADDRESS: " + device.getAddress());
+                discoveredDevices.add(device.getName() + "\n" + device.getAddress());
+
+                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.getStartedButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        /*
+         * ENABLE BLUETOOTH
+         */
+        final Button enableBTButton = (Button) findViewById(R.id.enableBT);
+        enableBTButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                 if (mBluetoothAdapter == null) {
                     // Device does not support Bluetooth
+                    Log.i ("BT_TEST_DEBUG", "Device does not support Bluetooth");
                 } else if (!mBluetoothAdapter.isEnabled()) {
                     Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            }
+        });
+
+        /*
+         * START DISCOVERY
+         */
+        final Button startDiscoveryButton = (Button) findViewById(R.id.startDiscovery);
+        startDiscoveryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                if (mBluetoothAdapter.isEnabled()) {
+                    // Register the BroadcastReceiver
+                    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+                    boolean isDiscovering = mBluetoothAdapter.startDiscovery();
+                    while (isDiscovering) {
+                        try {
+                            Thread.sleep(3000);
+                            isDiscovering = mBluetoothAdapter.isDiscovering();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    mBluetoothAdapter.cancelDiscovery();
+
                 }
             }
         });
